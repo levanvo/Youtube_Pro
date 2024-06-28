82fe764498971c9d2916daee45caa2210d96176e
Lê Văn Võ <vole543215@gmail.com>
2024-06-28 22:52:51 +0700

    getted alittle data Movies + path temp

--- FE/src/Web/Movies/Movies.jsx
+++ FE/src/Web/Movies/Movies.jsx
@@ -15,15 +15,21 @@ const Movies = () => {
         header && header.scrollIntoView({ behavior: 'smooth' })
     }
 
-    useEffect(() => {
+    useEffect( () => {
         const fetchMovies = async () => {
-            const { data } = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${import.meta.env.VITE_API_API_MOVIE}`);
-            setDataMovies(data?.results);
+            const result = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${import.meta.env.VITE_API_API_MOVIE}`);
+            const resultMovies = result.data?.results;
+
+            const ALL = await Promise.all(resultMovies?.map(async(item)=>{
+                const result = await axios.get(`https://api.themoviedb.org/3/movie/${item.id}?api_key=${import.meta.env.VITE_API_API_MOVIE}`);
+                return (result.data);
+            }));
+            setDataMovies(ALL);
         };
         fetchMovies();
     }, []);
 
-    console.log("dataMovies: ", dataMovies);
+    console.log("DATA: ",dataMovies);
     return (
         <div className="p-2 Top-Page w-[100%] h-[100%]">
             <div className='area_Movies flex flex-wrap justify-center w-[100%] h-[100%]'>
@@ -31,12 +37,12 @@ const Movies = () => {
                     dataMovies?.map((items, index) => {
                         return <div className="w-[27%] h-[40%] mx-7 relative" key={index}>
                             <Image className='rounded-md' src={`https://image.tmdb.org/t/p/w500/${items.backdrop_path}`} />
-                            <h2 className=' text-gray-600'>{items.title}</h2>
+                            <a href={`https://www.themoviedb.org/movie/${items.id}`} target="_blank"><h2 className=' text-gray-600'>{items.title}</h2></a>
                         </div>
                     })
                 }
             </div>
-            {scrollTop && <p onClick={() => scrollToTop()}><UpCircleOutlined className='text-white cursor-pointer fixed top-[92vh] right-[3vw]' style={{ scale: "2.5" }} /></p>}
+            {scrollTop && <p onClick={() => scrollToTop()}><UpCircleOutlined className='text-gray-300 cursor-pointer fixed top-[92vh] right-[3vw]' style={{ scale: "2.5" }} /></p>}
 
         </div>
     )
